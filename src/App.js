1735faad60055f9f02155619aa46fbde10922cd9
Egi Bhakti Bhuana <neargie@gmail.com>
2019-08-20 23:02:37 +0700

    add Route to the project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
@@ -6,6 +7,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* Switch get more control -> hanya menjalankan Route yang pertama apapun path yang ditulis di browser
+        exact untuk menampilkan sesuai url yang ditulis di browser
+        component untuk menampilkan halam yang akan ditampilkan
+      */}
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+      </Switch>
       <HomePage/>
     </div>
   );
